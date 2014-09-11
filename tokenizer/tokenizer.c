0907efa416eebefab845733f49732b66a6811628
Chris Fretz <cfretz244@gmail.com>
2014-09-11 12:22:54 -0400

    code replaces escape characters now. works on most edge cases

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -8,16 +8,119 @@ typedef struct TokenizerT_ {
     char *delimeters;
 } TokenizerT;
 
-int isDelimeter(char current, char *del) {
+const char *newline = "[0x0a]";
+const char *tab = "[0x09]";
+const char *vert_tab = "[0x0b]";
+const char *backspace = "[0x08]";
+const char *car_ret = "[0x0d]";
+const char *form_feed = "[0x0c]";
+const char *alert = "[0x07]";
+const char *backslash = "[0x5c]";
+const char *quote = "[0x22]";
+const int rep_len = 6;
+
+int isDelimeter(char *current, char *del) {
     // Iterate over all delimeter characters.
     while (*del != '\0') {
-        if (*del == current) {
-            return 1;
+        if (*del != '\\') {
+            if (*del == *current) {
+                return 1;
+            }
+        } else {
+            if (*del == *current && *(del + 1) == *(current + 1)) {
+                return 1;
+            }
+            del++;
         }
         del++;
     }
 
     return 0;
+}
+
+char *unescape(char *source, int length) {
+    // Make copies of necessary variables.
+    int total_length = length;
+    char *src_ptr = source;
+
+    // Calculate total length of string after making replacements.
+    for (int i = 0; i < length; i++) {
+        if (*src_ptr == '\\') {
+            char type = *(src_ptr + 1);
+            if (type == 'n' || type == 't' || type == 'v' || type == 'b' || type == 'r' || type == 'f' || type == 'a' ||
+                    type == '\\' || type == '"') {
+                total_length += rep_len - 2;
+            } else {
+                total_length--;
+            }
+        }
+        src_ptr++;
+    }
+
+    // Malloc space for expanded token and setup pointers.
+    char *expanded = (char *) malloc(sizeof(char) * total_length);
+    src_ptr = source;
+    char *dest_ptr = expanded;
+
+    // I hate the code duplication here, but we needed to know the size of the token before we could start working on it.
+    for (int i = 0; i < length; i++) {
+        if (*src_ptr == '\\') {
+            int offset = src_ptr - source;
+
+            // Copy over substring if there is one.
+            if (offset > 0) {
+                memcpy(dest_ptr, source, offset);
+                source = src_ptr;
+                dest_ptr += offset;
+            }
+
+            // Indentify character case and copy over bracketed hex.
+            switch (*(src_ptr + 1)) {
+                case 'n':
+                    memcpy(dest_ptr, newline, rep_len);
+                    break;
+                case 't':
+                    memcpy(dest_ptr, tab, rep_len);
+                    break;
+                case 'v':
+                    memcpy(dest_ptr, vert_tab, rep_len);
+                    break;
+                case 'b':
+                    memcpy(dest_ptr, backspace, rep_len);
+                    break;
+                case 'r':
+                    memcpy(dest_ptr, car_ret, rep_len);
+                    break;
+                case 'f':
+                    memcpy(dest_ptr, form_feed, rep_len);
+                    break;
+                case 'a':
+                    memcpy(dest_ptr, alert, rep_len);
+                    break;
+                case '\\':
+                    memcpy(dest_ptr, backslash, rep_len);
+                    break;
+                case '"':
+                    memcpy(dest_ptr, quote, rep_len);
+                    break;
+            }
+
+            // Advance pointers.
+            dest_ptr += rep_len;
+            source += 2;
+        }
+        src_ptr++;
+    }
+
+    if (source != src_ptr) {
+        int offset = src_ptr - source;
+        memcpy(dest_ptr, source, offset);
+        dest_ptr += offset;
+    }
+
+    *dest_ptr = '\0';
+
+    return expanded;
 }
 
 TokenizerT *TKCreate(char *separators, char *ts) {
@@ -56,19 +159,12 @@ char *TKGetNextToken(TokenizerT *tk) {
 
     // Loop over string characters until we find a delimeter.
     while (*str != '\0') {
-        if (isDelimeter(*str, del)) {
-            // Figure out how far we've gone and continue only if
-            // the generated token would have length > 0.
+        if (isDelimeter(str, del)) {
+            // Figure out how far we've gone and continue only if the generated token would have length > 0.
             int offset = str - tk->remaining;
             if (offset > 1) {
-                // Malloc space for token.
-                // I know that the size of char is defined to be one.
-                // I like being explicit.
-                token = (char *) malloc(sizeof(char) * (offset + 1));
-
-                // Copy token and null terminate it.
-                memcpy(token, tk->remaining, offset);
-                token[offset + 1] = '\0';
+                // Call unescape to parse out token and replace escape characters.
+                token = unescape(tk->remaining, offset);
 
                 // Advance the remaining pointer to update the struct.
                 tk->remaining = str + 1;
@@ -76,22 +172,29 @@ char *TKGetNextToken(TokenizerT *tk) {
                 // Return the found token.
                 return token;
             } else {
-                // Advance the remaining pointer to skip over worthless
-                // token if the size check fails.
-                tk->remaining = str + 1;
+                // Advance the remaining pointer to skip over worthless token if the size check fails.
+                if (*str == '\\') {
+                    tk->remaining = str + 2;
+                } else {
+                    tk->remaining = str + 1;
+                }
             }
         }
 
         // Consider the next character.
-        str++;
+        if (*str == '\\') {
+            str += 2;
+        } else {
+            str++;
+        }
     }
 
-    // No delimeter was found. Check if string is empty, meaning we're
-    // done, or if we still have one last token.
-    if (*tk->remaining != '\0') {
-        char *tmp = tk->remaining;
+    // No delimeter was found. Check if string is empty, meaning we're done, or if we still have one last token.
+    if (tk->remaining != str) {
+        int offset = str - tk->remaining;
+        char *token = unescape(tk->remaining, offset);
         tk->remaining = str;
-        return tmp;
+        return token;
     } else {
         return NULL;
     }
@@ -104,21 +207,18 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // Create tokenizer struct from given arguments and
-    // get the first token.
+    // Create tokenizer struct from given arguments and get the first token.
     TokenizerT *tokenizer = TKCreate(argv[1], argv[2]);
     char *token = TKGetNextToken(tokenizer);
 
-    // Continuously print the next token until there are
-    // none left.
+    // Continuously print the next token until there are none left.
     while (token != NULL) {
         printf("%s\n", token);
         token = TKGetNextToken(tokenizer);
     }
 
-    // Even though the memory will automatically be freed
-    // by the termination of the program, it seems good form
-    // to explicity free the tokenizer struct.
+    // Even though the memory will automatically be freed by the termination of the program, it seems good form to explicity
+    // free the tokenizer struct.
     TKDestroy(tokenizer);
 
     return 0;
